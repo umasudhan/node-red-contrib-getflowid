597c88bf1eb27477f9f7873bacdf0de6498e5214
umasudhan <umasudhan.p@gmail.com>
2019-12-13 11:37:57 +1100

    add flow name; change output field to flowinfo rather than overwriting msg.payload

---
+++ changes.md
@@ -0,0 +1,3 @@
+v 1.0.3 add flow name; change output field to flowinfo rather than overwriting msg.payload
+v 1.0.2 add readme
+v 1.0.1 initial version
--- getflowid.js
+++ getflowid.js
@@ -2,17 +2,21 @@
   Gets Flow ID
 */
 const os = require('os');
+const RED2 = require.main.require('node-red');
 
 module.exports = function (RED) {
     function getflowidnode(config) {
         RED.nodes.createNode(this, config);
         this.name = config.name;
-        var node = this;
+        const node = this;
         node.on("input", function (msg) {
-            msg.payload = {
+            const flow = RED2.nodes.getFlow(node.z);
+            const flowname = flow ? flow.label : '';
+            msg.flowinfo = {
                 nodeid: node.id,
                 flowid: node.z,
-                hostname: os.hostname()
+                hostname: os.hostname(),
+                flowname
             };
             node.send(msg);
         });
